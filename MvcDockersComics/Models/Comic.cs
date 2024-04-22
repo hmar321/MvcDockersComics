e1f77ca898e1ce8746315d348013973fe5c1e7d9
hmar321 <hmauricio.ar@gmail.com>
2024-04-22 14:08:10 +0200

    Add project files.

---
+++ MvcDockersComics/Models/Comic.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MvcDockersComics.Models
+{
+    [Table("COMICS")]
+    public class Comic
+    {
+        [Key]
+        [Column("IDCOMIC")]
+        public int IdComic { get; set; }
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+        [Column("IMAGEN")]
+        public string Imagen { get; set; }
+    }
+}
---
+++ MvcDockersComics/Repositories/RepositoryComics.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcDockersComics.Data;
+using MvcDockersComics.Models;
+
+namespace MvcDockersComics.Repositories
+{
+    public class RepositoryComics
+    {
+        private ComicsContext context;
+
+        public RepositoryComics(ComicsContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Comic>> GetComicsAsync()
+        {
+            List<Comic> comics = await this.context.Comics.ToListAsync();
+            return comics;
+        }
+        public async Task<Comic> FindComicAsync(int idcomic)
+        {
+            Comic comic = await this.context.Comics.FirstOrDefaultAsync(x => x.IdComic == idcomic);
+            return comic;
+        }
+
+        public async Task<int> GetNextIdComicAsync()
+        {
+            int nextid = await this.context.Comics.MaxAsync(x=>x.IdComic)+1;
+            return nextid;
+        }
+
+        public async Task InsertComicAsync(string nombre,string imagen)
+        {
+            Comic comic = new Comic();
+            comic.IdComic = await this.GetNextIdComicAsync();
+            comic.Nombre = nombre;
+            comic.Imagen = imagen;
+            this.context.Comics.Add(comic);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
